d8d614c7baabadaef688c53eb5cb72b22f4de22a
Tobiasz <tobi13072@gmail.com>
2023-11-19 19:56:27 +0100

    Initialize commit

---
+++ src/main/java/com/example/covidserver/CovidServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.covidserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidServerApplication.class, args);
+    }
+
+}
