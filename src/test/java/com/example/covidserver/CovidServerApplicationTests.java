57d354ac88e74fc39637682cd14f5ff612e53169
23122K <hazy.parader-0r@icloud.com>
2023-11-19 20:36:33 +0100

    tests

---
+++ src/test/java/com/example/covidserver/CovidServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covidserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
